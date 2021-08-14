6c80bad5c1ea7badf66fc81af1534bdfeeb40b6d
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-08-14 09:57:23 +0300

    Add data annotations to create dto

--- Dtos/ItemCreateDto.cs
+++ Dtos/ItemCreateDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Todo.Dtos
 {
     public class ItemCreateDto
     {
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Description { get; set; }
     }
 }
